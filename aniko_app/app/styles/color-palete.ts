25b0e5e55a5f3e90cdbc1384fc118334eb12fbff
ahkeyuuuhh <zitacristel@gmail.com>
2025-10-15 06:18:26 +0800

    color palette

---
+++ aniko_app/app/styles/color-palete.ts
@@ -0,0 +1,18 @@
+/*
+:root { --primary-green: #1D492C; 
+--accent-green: #84cc16; 
+--pastel-green: #BDE08A; 
+--light-green: #f0fdf4; 
+--dark-green: #143820; 
+--dark-gray: #374151; 
+--light-gray: #f9fafb; 
+--white: #ffffff; 
+--bg-color: #cfc4b2ff; 
+--primary-brown: #8A6440; 
+--dark-brown: #4D2D18; 
+--gradient-primary: linear-gradient(135deg, var(--primary-green), var(--accent-green)); 
+--gradient-secondary: linear-gradient(135deg, var(--primary-green), var(--pastel-green)); 
+--gradient-secondary-light: linear-gradient(135deg, var(--pastel-green), var(--light-green)); 
+--gradient-brown: linear-gradient(135deg, var(--primary-brown), var(--dark-brown)); 
+--gradient-brown-light: linear-gradient(135deg, var(--primary-brown), #c9b29e); }
+*/
